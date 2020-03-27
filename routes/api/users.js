f66cd96f0720e6435e920d1edf42cc5485108197
Naveed Panjwani <55307016+NaveedPanjwani@users.noreply.github.com>
2020-03-27 14:36:37 -0500

    changes to api/users

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-//@route GET api/users
-//@acess is public
-router.get('/', (req, res) => res.send('User route'));
+//route: POST api/users
+//description: registering users
+//access is public
+router.post('/', (req, res) => {
+  console.log(req.body);
+  res.send('User route');
+});
 module.exports = router;
