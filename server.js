370b2bf13b755fac088efdcb6c528484de75767a
Muba97 <mubakhan1997@gmail.com>
2020-03-28 14:48:44 -0500

    took the database stuff out

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+const PORT = 3000;
+
+app.get('/',(request,response) => {
+    response.send(`Node and express server is running on port ${PORT}`);
+});
+
+app.listen(PORT, () =>{
+    console.log(`Your server is running on port ${PORT}`);
+});
