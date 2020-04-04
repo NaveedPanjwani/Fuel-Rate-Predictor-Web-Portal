cf5242e93abb9ff5e9d64d162fa756255ba861b0
Naveed Panjwani <55307016+NaveedPanjwani@users.noreply.github.com>
2020-04-03 21:43:03 -0500

    Update apptest.js

--- test/apptest.js
+++ test/apptest.js
@@ -19,18 +19,18 @@ describe('auth', () => {
       })
       .catch((err) => done(err));
   });
-  it('Testing Login with username not in database', (done) => {
+  it('Testing Login with username not in database and getting error as response', (done) => {
     request(app)
       .post('/api/auth')
       .send({ username: 'wrongusername', password: 'passwordpassword' })
       .then((res) => {
         const body = res.body;
-        expect(body).to.contain.property('Username not in the System');
+        expect(body).to.contain.property('errors');
         done();
       })
       .catch((err) => done(err));
   });
-  it('Testing Login with wrong password', (done) => {
+  it('Testing Login with wrong password and getting error as response', (done) => {
     request(app)
       .post('/api/auth')
       .send({ username: 'robotrobot', password: 'wrongpassword' })
@@ -46,24 +46,148 @@ describe('auth', () => {
       })
       .catch((err) => done(err));
   });
-  it('Testing Login with short username', (done) => {
+  it('Testing Login with short username and getting error as response', (done) => {
     request(app)
       .post('/api/auth')
       .send({ username: 'short', password: 'wrongpassword' })
       .then((res) => {
         const body = res.body;
-        expect(body).to.contain.property('Please include valid email');
+        expect(body).to.contain.property('errors');
         done();
       })
       .catch((err) => done(err));
   });
-  it('Testing Login with short password', (done) => {
+  it('Testing Login with short password and getting error as response', (done) => {
     request(app)
       .post('/api/auth')
       .send({ username: 'robotrobot', password: 'small' })
       .then((res) => {
         const body = res.body;
-        expect(body).to.contain.property('Password is not correct');
+        expect(body).to.contain.property('errors');
+        done();
+      })
+      .catch((err) => done(err));
+  });
+});
+
+describe('Forum', () => {
+  it('Testing if Number is correctly inputted into Gallons', (done) => {
+    request(app)
+      .post(
+        '/api/forum/me?x-auth-token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoicm9ib3Ryb2JvdCJ9LCJpYXQiOjE1ODU5NTU0Mjh9._YSGDRLar5PbwfrKAfxpywn9OCOGmIjck1R70xFFS0k'
+      )
+      .send({ Gallons_Requested: 1111, Delivery_Date: 'fvdffdvffdv' })
+      .then((res) => {
+        const body = res.body;
+        expect(body).to.contain.property('price');
+        done();
+      })
+      .catch((err) => done(err));
+  });
+  it('Testing if Date is correctly not empty', (done) => {
+    request(app)
+      .post(
+        '/api/forum/me?x-auth-token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoicm9ib3Ryb2JvdCJ9LCJpYXQiOjE1ODU5NTU0Mjh9._YSGDRLar5PbwfrKAfxpywn9OCOGmIjck1R70xFFS0kapi/forum/me?x-auth-token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoicm9ib3Ryb2JvdCJ9LCJpYXQiOjE1ODU5NTU0Mjh9._YSGDRLar5PbwfrKAfxpywn9OCOGmIjck1R70xFFS0k'
+      )
+      .send({ Gallons_Requested: 1111, Delivery_Date: 'fvdffdvffdv' })
+      .then((res) => {
+        const body = res.body;
+        expect(body).to.contain.property('price');
+        done();
+      })
+      .catch((err) => done(err));
+  });
+  it('Testing if Number is not inputted into Gallons and getting error as response', (done) => {
+    request(app)
+      .post(
+        '/api/forum/me?x-auth-token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoicm9ib3Ryb2JvdCJ9LCJpYXQiOjE1ODU5NTU0Mjh9._YSGDRLar5PbwfrKAfxpywn9OCOGmIjck1R70xFFS0k'
+      )
+      .send({ Gallons_Requested: 1111, Delivery_Date: 'fvdffdvffdv' })
+      .then((res) => {
+        const body = res.body;
+        expect(body).to.contain.property('error');
+        done();
+      })
+      .catch((err) => done(err));
+  });
+  it('Testing if Date is empty and getting error as response', (done) => {
+    request(app)
+      .post(
+        '/aapi/forum/me?x-auth-token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoicm9ib3Ryb2JvdCJ9LCJpYXQiOjE1ODU5NTU0Mjh9._YSGDRLar5PbwfrKAfxpywn9OCOGmIjck1R70xFFS0k'
+      )
+      .send({ Gallons_Requested: 1111, Delivery_Date: 'fvdffdvffdv' })
+      .then((res) => {
+        const body = res.body;
+        expect(body).to.contain.property('error');
+        done();
+      })
+      .catch((err) => done(err));
+  });
+});
+
+describe('Registration', () => {
+  it('Testing if Username is less than 7 in length and getting error as response', (done) => {
+    request(app)
+      .post('/api/user/add')
+      .send({ username: 'robot', password: 'passwordpassword' })
+      .then((res) => {
+        const body = res.body;
+        expect(body).to.contain.property('errors');
+        done();
+      })
+      .catch((err) => done(err));
+  });
+  it('Testing if Username is more than 7 in length', (done) => {
+    request(app)
+      .post('/api/user/add')
+      .send({ username: 'robotrobotrobot', password: 'passwordpassword' })
+      .then((res) => {
+        const body = res.body;
+        expect(body).to.contain.property('message');
+        done();
+      })
+      .catch((err) => done(err));
+  });
+  it('Testing if Password is less than 7 in length and getting error as response', (done) => {
+    request(app)
+      .post('/api/user/add')
+      .send({ username: 'robotrobot', password: 'pass' })
+      .then((res) => {
+        const body = res.body;
+        expect(body).to.contain.property('errors');
+        done();
+      })
+      .catch((err) => done(err));
+  });
+  it('Testing if Password is more than 7 in length', (done) => {
+    request(app)
+      .post('/api/user/add')
+      .send({ username: 'robotrobot', password: 'passwordpasswordpassword' })
+      .then((res) => {
+        const body = res.body;
+        expect(body).to.contain.property('message');
+        done();
+      })
+      .catch((err) => done(err));
+  });
+  it('Testing if Username is not currently in the database', (done) => {
+    request(app)
+      .post('/api/user/add')
+      .send({ username: 'robotrobot2', password: 'passwordpassword' })
+      .then((res) => {
+        const body = res.body;
+        expect(body).to.contain.property('message');
+        done();
+      })
+      .catch((err) => done(err));
+  });
+  it('Testing if Username is currently in the database and getting error as response', (done) => {
+    request(app)
+      .post('/api/user/add')
+      .send({ username: 'robotrobot', password: 'passwordpassword' })
+      .then((res) => {
+        const body = res.body;
+        expect(body).to.contain.property('message');
         done();
       })
       .catch((err) => done(err));
