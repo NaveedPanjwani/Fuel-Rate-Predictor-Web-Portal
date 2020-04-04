b9c7a295aee905f5de47ebadcf0b9b38d9dd023c
Muba97 <mubakhan1997@gmail.com>
2020-04-03 21:11:39 -0500

    fixing merge issues

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const forum = require('./routes/api/forum');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.use(cors({ origin: '*' }));
 // just usin cors() should work but not sure why its not working rn
---
+++ test/api/usertest.js
@@ -0,0 +1,1 @@
+let chai = require('chai');
