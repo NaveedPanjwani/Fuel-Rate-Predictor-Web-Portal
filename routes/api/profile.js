a3b0e4f07bff9cc63b553e93065e89c147e9685a
Naveed Panjwani <55307016+NaveedPanjwani@users.noreply.github.com>
2020-04-20 00:16:20 -0500

    yeah do it

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -12,6 +12,7 @@ const Profile = require('../../models/profile_model');
 // description:Authenticate user and get token
 // access: public:
 
+/*
 router.post('/', auth, async (req, res) => {
   try {
     let user = await Profile.findOne({ username: req.user.id });
@@ -29,10 +30,46 @@ router.post('/', auth, async (req, res) => {
     }
     if (user) {
     }
-    return res.status(200).json({ message: 'OK', forum });
+    return res.status(200).json({ message: 'OK', user });
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server error');
   }
 });
+*/
+//let user = Profile.findOne({ username: req.user.id });
+//if (!user) {
+router.post('/', auth, async (req, res) => {
+  try {
+    const { fullname, address, address2, city, state, zipcode } = req.body;
+    //let user = User.findOne({ username: req.user.id });
+    profile = await Profile.create({
+      username: req.user.id,
+      fullname,
+      address,
+      address2,
+      city,
+      state,
+      zipcode,
+    });
+    return res.status(200).json({ message: 'OK', profile });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+//} else {
+router.get('/', auth, async (req, res) => {
+  try {
+    const profile = Profile.findOne({ username: req.user.id });
+    //res.send(profile);
+    console.log(profile);
+    //return res.status(200).json({ message: 'OK', profile });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+//}
+
 module.exports = router;
